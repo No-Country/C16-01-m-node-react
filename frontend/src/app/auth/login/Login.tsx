cc2d46ff977e99c9d49943370b3f3e69d8dfafc4
Ignacio Guridi <nacho_g88@hotmail.com>
2024-02-22 22:56:55 -0300

    modificaciones login

--- frontend/src/app/auth/login/Login.tsx
+++ frontend/src/app/auth/login/Login.tsx
@@ -2,13 +2,12 @@
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-// import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline'; // Importa los iconos
 
 export default function Login() {
   const [errors, setErrors] = useState<string[]>([]);
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const [showPassword, setShowPassword] = useState<boolean>(false); // Nuevo estado para mostrar/ocultar contraseña
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -38,22 +37,59 @@ export default function Login() {
         name="email"
         placeholder="E-mail"
         value={email}
+        onChange={(e) => setEmail(e.target.value)}
         className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md text-black"
       />
-      <div className="relative"> {/* Envuelve el input en un div relativo */}
+      <div className="relative">
         <input
-          type={showPassword ? "text" : "password"} // Cambia el tipo de entrada basado en el estado showPassword
+          type={showPassword ? "text" : "password"}
           placeholder="Contraseña"
           required
           name="password"
           value={password}
-          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md text-black pr-10" // Añade padding a la derecha para el icono
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md text-black pr-10"
         />
-        <div 
-          className="absolute top-3 right-0 pr-3 flex items-center cursor-pointer" // Posiciona el icono en la esquina superior derecha
-          onClick={() => setShowPassword(!showPassword)} // Cambia el estado showPassword cuando se hace click en el icono
+        <div
+          className="absolute top-3 right-0 pr-3 flex items-center cursor-pointer"
+          onClick={() => setShowPassword(!showPassword)}
         >
-          {/* {showPassword ? <EyeIcon className="h-5 w-5" /> : <EyeOffIcon className="h-5 w-5" />} Muestra el icono correcto basado en el estado showPassword */}
+          {showPassword ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+              />
+            </svg>
+          )}
         </div>
       </div>
       <div className="gap-2">
@@ -65,8 +101,7 @@ export default function Login() {
         </button>
       </div>
       <button
-        className="w-full bg-blue-500 text-white px-4 py-2 rounded-full
-          hover:bg-blue-600 transition-colors mb-2"
+        className="w-full bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition-colors mb-2"
       >
         Crear Cuenta
       </button>
